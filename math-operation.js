7bb1772b63d5ac04a9695aa49cd139756b6c1ca3
saifulhaque015 <saiful015.uiu@gmail.com>
2022-03-24 00:47:49 +0600

    math operation

---
+++ math-operation.js
@@ -0,0 +1,15 @@
+var price1 = 21;
+var price2 = 10;
+
+var total = price1 + price2;
+var difference = price1 - price2;
+var multiply = price1 * price2;
+var division = price1 / price2;
+
+// console.log(total);
+// console.log(difference);
+// console.log(multiply);
+// console.log(division);
+
+var newPrice = price1 * 3;
+console.log(newPrice);
