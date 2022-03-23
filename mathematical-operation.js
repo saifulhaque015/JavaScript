1fc892d5b07f2b06878f25d69ffba479619098de
saifulhaque015 <saiful015.uiu@gmail.com>
2022-03-24 00:28:27 +0600

    multiplication added

--- mathematical-operation.js
+++ mathematical-operation.js
@@ -1,8 +1,11 @@
 var onionPrice = 52;
 var eggPrice = 38;
+var eggQty = 5;
 var sum = onionPrice + eggPrice;
 // console.log(onionPrice);
 // console.log(eggPrice);
 // console.log(sum);
 var priceDifference = onionPrice - eggPrice;
-console.log(priceDifference);
+// console.log(priceDifference);
+var totalEggPrice = eggPrice * eggQty;
+console.log(totalEggPrice);
