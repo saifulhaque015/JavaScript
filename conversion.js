ac5953e2a5eb4e10cef4ca254acd2027ae5102c7
saifulhaque015 <saiful015.uiu@gmail.com>
2022-03-25 23:26:18 +0600

    conversion added

---
+++ conversion.js
@@ -0,0 +1,24 @@
+var firstName = 'jenny'
+var lastName = 'grande'
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
+
+var first = 'saiful';
+var last = 'haque';
+var name = first + ' ' + last;
+console.log(name);
+
+// integer
+var sunglass = 3;
+// float
+var rice = 1.5;
+
+var onionPrice = '52';
+var eggPrice = '46';
+var con = parseInt(onionPrice);
+var conv = parseInt(eggPrice);
+console.log(con);
+console.log(conv);
+var total = conv + con;
+console.log(total);
+
