9d2aa5b6f672896e9acbdc7681da8e62b097f21f
saifulhaque015 <saiful015.uiu@gmail.com>
2022-03-25 23:32:47 +0600

    variable-type added

---
+++ variable-type.js
@@ -0,0 +1,9 @@
+var tShirtPrice = 200;
+console.log(tShirtPrice);
+console.log(typeof tShirtPrice);
+var tShirtPrice2 = '300';
+console.log(tShirtPrice2);
+console.log(typeof tShirtPrice2);
+var isHot = true;
+console.log(isHot);
+console.log(typeof isHot);
